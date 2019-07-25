7b8443a3e4bfd03db182605e247df7c8f6375e3d
jakecren <51045803+jakecren@users.noreply.github.com>
2019-07-25 23:12:15 +1000

    Database login capabilities added

--- flaskBlog/__init__.py
+++ flaskBlog/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "6441b3dbdb388ba0de5c36ebe93a32e1"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from flaskBlog import routes
