ed8776180e73bb4c93af3c62b6b4ee1b0fbba169
jakecren <51045803+jakecren@users.noreply.github.com>
2019-08-03 21:59:06 +1000

    Updated python files to PEP8 compliance using flake8

--- flaskBlog/forms.py
+++ flaskBlog/forms.py
@@ -7,21 +7,25 @@ from flaskBlog.models import User
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired(), Length(min=2, max=20)])
+    username = StringField("Username", validators=[
+                           DataRequired(), Length(min=2, max=20)])
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
-    confirmPassword = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
+    confirmPassword = PasswordField("Confirm Password", validators=[
+                                    DataRequired(), EqualTo("password")])
     submit = SubmitField("Sign Up")
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError("Username is unavailable, please choose a different username.")
+            raise ValidationError(
+                "Username is unavailable, please choose a different username.")
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError("Email is unavailable, please choose a different username.")
+            raise ValidationError(
+                "Email is unavailable, please choose a different username.")
 
 
 class LoginForm(FlaskForm):
@@ -30,23 +34,29 @@ class LoginForm(FlaskForm):
     remember = BooleanField("Remember Me")
     submit = SubmitField("Log In")
 
+
 class UpdateAccountForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired(), Length(min=2, max=20)])
+    username = StringField("Username", validators=[
+                           DataRequired(), Length(min=2, max=20)])
     email = StringField("Email", validators=[DataRequired(), Email()])
-    picture = FileField("Update Profile Picture", validators=[FileAllowed(["jpg", "png"])])
+    picture = FileField("Update Profile Picture", validators=[
+                        FileAllowed(["jpg", "png"])])
     submit = SubmitField("Update Details")
 
     def validate_username(self, username):
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
             if user:
-                raise ValidationError("Username is unavailable, please choose a different username.")
+                raise ValidationError(
+                    "Username is unavailable, please choose a different username.")
 
     def validate_email(self, email):
         if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()
             if user:
-                raise ValidationError("Email is unavailable, please choose a different username.")
+                raise ValidationError(
+                    "Email is unavailable, please choose a different username.")
+
 
 class PostForm(FlaskForm):
     title = StringField("Title", validators=[DataRequired()])
--- flaskBlog/routes.py
+++ flaskBlog/routes.py
@@ -97,17 +97,20 @@ def account():
 def new_post():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        post = Post(title=form.title.data,
+                    content=form.content.data, author=current_user)
         db.session.add(post)
         db.session.commit()
         flash("Your post has been created!", "success")
         return redirect(url_for("home"))
     return render_template("create_post.html", title="New Post", form=form)
 
+
 @app.route("/post/<int:post_id>")
 def post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template("post.html", title=post.title, post=post, legend="New Post")
+
 
 @app.route("/post/<int:post_id>/update", methods=["GET", "POST"])
 @login_required
@@ -121,7 +124,7 @@ def update_post(post_id):
         post.content = form.content.data
         db.session.commit()
         flash("Your post has been updated!", "success")
-        return redirect(url_for("post", post_id = post.id))
+        return redirect(url_for("post", post_id=post.id))
     elif request.method == "GET":
         form.title.data = post.title
         form.content.data = post.content
