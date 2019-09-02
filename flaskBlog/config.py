5ced453ef4d1395e3fb8a5fb9e31ae266c62188d
jakecren <51045803+jakecren@users.noreply.github.com>
2019-09-02 22:57:09 +1000

    Updated .gitignore and requirements.txt added

--- flaskBlog/config.py
+++ flaskBlog/config.py
@@ -1,8 +1,5 @@
 class Config():
     SECRET_KEY = "6441b3dbdb388ba0de5c36ebe93a32e1"
     SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
-    MAIL_SERVER = "smtp.sendgrid.net"
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
     MAIL_USERNAME = "apikey"
     MAIL_PASSWORD = "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"
