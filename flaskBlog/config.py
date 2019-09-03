7e516092645c8d6e14ca4dd6a0012f672e35f7b0
jakecren <51045803+jakecren@users.noreply.github.com>
2019-09-04 03:51:50 +1000

    Hard-Coded SqlAlchemyURL

--- flaskBlog/config.py
+++ flaskBlog/config.py
@@ -2,6 +2,6 @@ import os
 
 class Config():
     SECRET_KEY = os.environ.get("ATCBlogSite_SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("ATCBlogSite_SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
     MAIL_USERNAME = os.environ.get("ATCBlogSite_MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("ATCBlogSite_MAIL_PASSWORD")
